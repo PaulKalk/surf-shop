2efea5799ddffb9715018fd84eda5c3b7e56f10f
Ivan Andrijevic <ivan3.andrijevic@protonmail.com>
2023-01-27 10:13:36 +0100

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 /* GET index /posts. */
 router.get("/", (req, res, next) => {
   res.send("index/posts");
@@ -9,18 +10,22 @@ router.get("/", (req, res, next) => {
 router.get("/new", (req, res, next) => {
   res.send("NEW/posts/new");
 });
+
 /*POST create posts/  *-creating route for rendering our post*/
 router.post("/", (req, res, next) => {
   res.send("CREATE /posts");
 });
+
 /*Get show posts/:id   */
 router.get("/:id", (req, res, next) => {
   res.send("SHOW/posts/:id");
 });
+
 /*Get edit posts/:id/edit  */
 router.get("/:id/edit", (req, res, next) => {
   res.send("EDIT /posts/:id/edit");
 });
+
 /*Get update posts/:id */
 router.put("/:id", (req, res, next) => {
   res.send("EDIT posts/:id");
