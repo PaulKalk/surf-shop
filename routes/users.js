7b54c9110a565e1d146c25c9439d793f0e805220
Ivan Andrijevic <ivan3.andrijevic@protonmail.com>
2023-01-30 09:23:30 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", (req, res, next) => {
+  res.send("index");
+});
+
+/* GET users /register. */
+router.get("/register", (req, res, next) => {
+  res.send("GET /register");
+});
+
+/* POST users /register. */
+router.post("/register", (req, res, next) => {
+  res.send(" POST /register");
+});
+
+/* GET users /login. */
+router.get("/login", (req, res, next) => {
+  res.send("GET /login");
+});
+
+/* POST  /login. */
+router.post("/login", (req, res, next) => {
+  res.send("POST /login");
+});
+
+/* GET  /profile. */
+router.get("/profile", (req, res, next) => {
+  res.send("POST /users/profile");
+});
+/* PUT  /profile. */
+router.put("/profile/:user_id", (req, res, next) => {
+  res.send("PUT /profile/:user_id");
+});
+
+/* GET  /forgot. */
+router.get("/forgot", (req, res, next) => {
+  res.send("GET /forgot");
+});
+/* PUT  /forgot. */
+router.put("/forgot", (req, res, next) => {
+  res.send("PUT /forgto");
+});
+/* GET  /reset-pw. */
+router.get("/reset/:token", (req, res, next) => {
+  res.send("GET /reset/:token");
+});
+/* PUT  /reset-pw. */
+router.put("/reset/:token", (req, res, next) => {
+  res.send("PUT /reset/:token");
 });
 
 module.exports = router;
